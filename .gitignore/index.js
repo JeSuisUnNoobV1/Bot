5fb21c86c5fef8940c228f1d91a8be5b4ffd6ed6
JeSuisUnNoobV1 <45183326+JeSuisUnNoobV1@users.noreply.github.com>
2018-12-02 17:06:49 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,94 +2,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('Rinaorc en GolemRush', [0]);
+    client.user.setActivity('Minecraft - Rinaorc', [0]);
     console.log('Le bot a été charger avec succès !');
 });
 
-// NB
+client.on("guildMemberAdd", member => {
+    member.guild.channels.find("name", "bienvenue").send("Bienvenue à toi sur le serveur de la NoobIse !");
+});
+
 client.on('message', msg => {
-  if (msg.content === 'nb') {
-    msg.channel.send("NoobBot"),
-    msg.channel.send("Version : Alpha 0.4 "),
-    msg.channel.send("Développé par : legameur6810");
+  if (msg.content === '$info') {
+    msg.channel.send("```Makerdown\nNoobBot \nVersion : Beta 1.8\nCréateur : legameur6810#4488 \nSortie le : 20s/11/2018```");
   }
 });
-
-// GOLEMRUSH
-client.on('message', msg => {
-    if (msg.content === 'nb/golemrush') {
-        msg.reply("Le golemrush est un mode de jeu disponible sur le serveur minecraft rinaorc. Le but du jeu est : Deux team de 5 s'affronte (5v5). Une ile pour chaque équipe et une ile centrale. Chaque équipe dispose d'un golem. Au millieu un golem (GolemBbooster) donne un effet (sois 2x plus d'oir ou 2x plus d'émeraude) à l'équipe qui le tue. L'effet dure 30secondes.");
-    }
-});
-
-// RINAORC
-client.on('message', msg => {
-    if (msg.content === 'nb/rinaorc') {
-        msg.reply("Ip du serveur : play.rinaorc.com"),
-        msg.reply("Voir le site de Rinaorc : https://rinaorc.com");
-    }
-});
-client.on('message', msg => {
-    if (msg.content === 'nb/rina') {
-        msg.reply("Ip du serveur : play.rinaorc.com"),
-        msg.reply("Voir le site de Rinaorc : https://rinaorc.com");
-    }
-});
-
-// FORM
-client.on('message', msg => {
-    if (msg.content === 'nb/recrutement') {
-        msg.reply("Voici le forumlaire : https://goo.gl/MVoxY4")
-    }
-});
-
-// HELP
-client.on('message', msg => {
-    if (msg.content === 'nb/help') {
-        msg.reply("Tu peut voir la documentation ici : http://textup.fr/297296vF")
-    }
-});
-
-client.on('message', msg => {
-    if (msg.content === 'nb/aide') {
-        msg.reply("Tu peut voir la documentation ici : http://textup.fr/297296vF")
-    }
-});
-
-// CHANGELOGS
-
-client.on('message', msg => {
-    if (msg.content === 'nb/change') {
-        msg.reply("Voir les nouveauté ici : http://textup.fr/297591Hz")
-    }
-});
-client.on('message', msg => {
-    if (msg.content === 'nb/changelogs') {
-        msg.reply("Voir les nouveauté ici : http://textup.fr/297591Hz")
-    }
-});
-
-// Regle
-client.on('message', msg => {
-    if (msg.content === 'nb/regles') {
-        msg.reply('Voici les règles de la team : http://textup.fr/297606fw')
-    }
-});
-client.on('message', msg => {
-    if (msg.content === 'nb/regle') {
-        msg.reply('Voici les règles de la team : http://textup.fr/297606fw')
-    }
-});
-client.on('message', msg => {
-    if (msg.content === 'nb/rule') {
-        msg.reply('Voici les règles de la team : http://textup.fr/297606fw')
-    }
-});
-client.on('message', msg => {
-    if (msg.content === 'nb/règle') {
-        msg.reply('Voici les règles de la team : http://textup.fr/297606fw')
-    }
-});
-
 
 client.login(process.env.TOKEN);
