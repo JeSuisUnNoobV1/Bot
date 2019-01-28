88bb983c73abd7b82228f3865b1e2d385c4a7b6e
JeSuisUnNoobV1 <45183326+JeSuisUnNoobV1@users.noreply.github.com>
2019-01-28 20:12:05 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -30,7 +30,7 @@ client.on('message', msg => {
   if (msg.content === '$info') {
     msg.channel.send(({embed: {
       color : 12745742,
-      description: "Version : Beta 1.9.3\nCréateur : legameur6810#4488 \nSortie le : 20/11/2018"
+      description: "Version : Beta 1.9.3\nCréateur : legameur6810#4488 \nEdité : Théotime#6461 \nSortie le : 20/11/2018"
     }}))
   }
 });
