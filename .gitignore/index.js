71ba9151410a40a401cc27f81fdd08bfd4bbded8
theotime-me <42675516+theotime-me@users.noreply.github.com>
2019-01-29 17:44:58 +0100

    Add files via upload

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -34,7 +34,7 @@ client.on('message', msg => {
       color: 12745742,
       description: "Bah ça va les insultes !"
     }}));
-  } else if (msg.content === 'how to embed') {
+  } else if (m === 'how to embed') {
     // We can create embeds using the MessageEmbed constructor
     // Read more about all that you can do with the constructor
     // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
@@ -47,46 +47,6 @@ client.on('message', msg => {
       .setDescription('Hello, this is a slick embed!');
     // Send the embed to the same channel as the message
     message.channel.send(embed);
-  }
-
-  switch (msg.content) {
-    case "Roboto help": case "Roboto -h":
-    	msg.reply(({embed: {
-			color: 12745742,
-			description: "Voici les commandes disponible :\n`$help` Donne toutes les commandes disponible\n`$aide` Donne toutes les commandes disponible\n`$info`  Donne des infos sur le bot"
-		}}));
-	break;
-	case "embed test":
-	message.channel.send({embed: {
-		color: 3447003,
-		author: {
-		  name: client.user.username,
-		  icon_url: client.user.avatarURL
-		},
-		title: "This is an embed",
-		url: "http://google.com",
-		description: "This is a test embed to showcase what they look like and what they can do.",
-		fields: [{
-			name: "Fields",
-			value: "They can have different fields with small headlines."
-		  },
-		  {
-			name: "Masked links",
-			value: "You can put [masked links](http://google.com) inside of rich embeds."
-		  },
-		  {
-			name: "Markdown",
-			value: "You can put all the *usual* **__Markdown__** inside of them."
-		  }
-		],
-		timestamp: new Date(),
-		footer: {
-		  icon_url: client.user.avatarURL,
-		  text: "© Example"
-		}
-	  }
-	});
-	break;
   }
 });
 
