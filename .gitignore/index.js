2e9d0287186ba823bd4d4788517264dbe07ea8c3
theotime-me <42675516+theotime-me@users.noreply.github.com>
2019-01-29 17:13:55 +0100

    Add files via upload

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,12 +5,13 @@ const activities_list = [
   "Des questions ?", 
   "Un projet ?",
   "Un site internet ?",
-  "Roboto est à votre disposition"
+  "De l'aide automatique",
+  "à votre disposition"
   ];
 
 // START
 client.on('ready', () => {
-    console.log('Le bot a été charger avec succès !');
+    console.log('Roboto ready');
     setInterval(() => {
       const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
       client.user.setActivity(activities_list[index]);
@@ -30,12 +31,25 @@ client.on('message', msg => {
     msg.reply(({embed: {
       color: 12745742,
       description: "Voici les commandes disponible :\n`$help` Donne toutes les commandes disponible\n`$aide` Donne toutes les commandes disponible\n`$info`  Donne des infos sur le bot"
-    }}))
+    }}));
   } else if(msg.content === "Abruti") {
     msg.reply(({embed: {
       color: 12745742,
       description: "Bah ça va les insultes !"
-    }}))
+    }}));
+  } else if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
   }
 });
 
