c292ecec7de81af93e0d5145bc548a739c984992
theotime-me <42675516+theotime-me@users.noreply.github.com>
2019-02-04 20:00:08 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -48,9 +48,18 @@ if (m == "roboto") {
   questionName = true;
 }
 
-if (m.indexOf('je suis') === 0 && questionName == true) {
-  msg.channel.send("D'accord "+m.replace('je suis ', '')+", maintenant, que voulez-vous ?");
+if (questionName == true) {
+  msg.channel.send("D'accord "+m.replace(/je suis |mon nom est |moi c'est |je m'appelle /, '')+", maintenant, que voulez-vous ?");
   questionName = false;
+  questionWish = true;
+}
+
+if (questionWish == true) {
+  if (m=="l'aide"||"de l'aide"||"j'ai besoin d'aide"||"aide"||"help"||"je veux de l'aide"||"aide moi"||"aide-moi") {
+    msg.react(":wink:");
+    msg.reply("Très bien, je t'ai tout envoyé en privé.");
+    questionWish = false;
+  }
 }
 });
 
