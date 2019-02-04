1e6cfa804613ac4e9db04ed1ad753f720adcc45e
theotime-me <42675516+theotime-me@users.noreply.github.com>
2019-02-04 19:33:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,6 +9,8 @@ const activities_list = [
   "votre disposition"
   ];
 
+var questionName = false;
+
 // START
 client.on('ready', () => {
     console.log('Roboto ready');
@@ -23,22 +25,32 @@ client.on('ready', () => {
 client.on("guildMemberAdd", members => {
     members.guild.channels.find("id", "539842703467479051").send("Bienvenue à **" + members.displayName +"**, tu es maintenant connecté au serveur discord !\n Pour avoir accès accès à tout le serveur discord, merci de suivre les instructions disponible à l'url suivant : https://modulobot.xyz/verify/539738715707408385");
     members.createDM().then(channel => {
-      return channel.send('Bienvenue **' + members.displayName+ "**,\n Tu as maintenant accès au serveur de Théotime ! \n\n _Cordialement, le staff_")
+      return channel.send('Bienvenue **' + members.displayName+ "**,\n Tu as maintenant accès au serveur de Théotime ! \n\n _Cordialement, le staff_");
    });
 });
 
 client.on('message', msg => {
 	m = msg.content.toLowerCase();
 
-if(m=="abruti" ||m=="con"||m=="connard"||m=="fdp"||m=="batard"||m=="pute" || m=="bese" || m=="beze" || m=="ntm" || m=="nique") {
+if(m.indexOf("abruti") != undefined ||m.indexOf("con") != undefined||m.indexOf("connard") != undefined||m.indexOf("fdp") != undefined||m.indexOf("batard") != undefined||m.indexOf("pute") != undefined || m.indexOf("bese") != undefined || m.indexOf("beze") != undefined || m.indexOf("ntm") != undefined || m.indexOf("nique")) {
     msg.channel.send({embed: {
   color: 16057630,
-  description: "Mais, c'est pas très très gentils !!"
+  description: "Hop Hop Hop, évitez les insultes s'il vous plait."
 }});
 msg.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
   .catch(console.error);
-  }
+}
+
+if (m == "roboto") {
+  msg.channel.send("Oui ? c'est moi. \n Je peux vous aidez si vous en avez besoin. \n Mais avant de commencer, qui êtes-vous ?");
+  questionName = true;
+}
+
+if (m.indexOf('je suis') === 0 && questionName == true) {
+  msg.channel.send("D'accord "+m.replace('je suis ', '')+", maintenant, que voulez-vous ?");
+  questionName = false;
+}
 });
 
 
