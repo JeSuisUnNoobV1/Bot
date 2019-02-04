00b74bd0a5aab2e20b853319cbf8b2ecf57a9ba1
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-02-04 10:59:57 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,6 +8,33 @@ const activities_list = [
   "De l'aide automatique",
   "à votre disposition"
   ];
+/* EMBED */
+const embedInsulte = new Discord.RichEmbed()
+  .setTitle("Mais")
+  .setAuthor("Roboto", "https://i.imgur.com/lm8s41J.png")
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 1024 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+
 
 // START
 client.on('ready', () => {
@@ -31,9 +58,7 @@ client.on('message', msg => {
 	m = msg.content.toLowerCase();
 
 if(m=="abruti"||m=="con"||m=="connard"||m=="fdp"||m=="batard"||m=="pute") {
-    msg.send(({embed: {
-      description: "Bah ça va les insultes !"
-    }}).setColor(0x33389c));
+    msg.channel.send({embed});
 msg.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
   .catch(console.error);
