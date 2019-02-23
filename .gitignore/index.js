5cb36e8abf887ad6d78b43ef372b202f219c9ae2
theotime-me <42675516+theotime-me@users.noreply.github.com>
2019-02-23 18:33:35 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,6 +9,7 @@ const	Discord = require('discord.js'),
 		users = require('./users.json'),
 		config = require('./config.json'),
 		client = new Discord.Client(),
+		guild = client.guilds.get("539738715707408385"),
 
 activities_list = [
   "",
@@ -78,10 +79,10 @@ client.on('ready', () => {
 /* 03 / new user
 ==================== */
 client.on("guildMemberAdd", members => {
-	members.addRole("Utilisateur discord");
+	members.addRole(guild.roles.find("name", "Utilisateur discord"));
     members.createDM().then(channel => {
     	channel.send('Bienvenue **' + members.displayName+ "**,\n Tu as maintenant accès au serveur discord \"Théotime.me\" !\nOn y parle de développement, de graphisme, d'ilustration et bien d'autres activités ! Ainsi chacun pourra parler de ses projets pour les faire évoluer. Si vous souhaitez inviter quelqu'un, utilisez ce lien: https://theotime.me/discord \n\n Amicalement, Roboto.");
-	});
+	});"Utilisateur discord"
 
 	let exist = false;
 
