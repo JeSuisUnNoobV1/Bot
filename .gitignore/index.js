69557052c82bf29f02f43893774441e40527124d
theotime-me <42675516+theotime-me@users.noreply.github.com>
2019-03-11 17:48:53 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -723,7 +723,6 @@ if (isAuth()){ // Il faut être autorisé à utiliser Roboto
 	if (m.startsWith(prefix+'github ') || m.startsWith("https://github.com/")) {
 		let q = msg.content.replace(prefix+'github ', "");
 			q = q.replace('https://github.com/', "");
-			q = q.split("/")[0]+q.split("/")[1].split('/')[0];
 
 		msg.delete();
 		if (q.includes('/')) {
